adc90ae2f4e6edb857822cc46dffa80fbbfeb3dd
BugsDomi <bugsdomi@neuf.fr>
2019-07-11 19:40:39 +0200

    Correction encodage

--- app.js
+++ app.js
@@ -12,16 +12,17 @@ app.get('/', function (req, res) {
 io.sockets.on('connection', function (socket, pseudo) {
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
     socket.on('nouveau_client', function(pseudo) {
-        pseudo = ent.encode(pseudo);
+        // pseudo = ent.encode(pseudo);
         socket.pseudo = pseudo;
         socket.broadcast.emit('nouveau_client', pseudo);
     });
 
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
-        message = ent.encode(message);
+        // message = ent.encode(message);
         socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
     }); 
 });
 
 server.listen(8080);
+ 
